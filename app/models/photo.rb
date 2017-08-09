ce164c723bd355caa9382a5ab13c1cacdd745ed6
nao <8beesoftware@gmail.com>
2017-08-10 01:22:00 +0900

    change

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :listing
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :cloudinary
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :cloudinary, :cloudinary_credentials => Rails.root.join("config/cloudinary.yml")
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
