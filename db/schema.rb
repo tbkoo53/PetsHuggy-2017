e9258e1aeefb4c393aa868639b5b2868df758d55
nao <8beesoftware@gmail.com>
2017-08-23 21:48:20 +0900

    add nav 【bug】default_image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823101542) do
+ActiveRecord::Schema.define(version: 20170823121543) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "home_type"
@@ -78,6 +78,10 @@ ActiveRecord::Schema.define(version: 20170823101542) do
     t.string   "name"
     t.string   "phone_number"
     t.string   "description"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
