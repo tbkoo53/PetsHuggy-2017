afe465ee189a19816d22795db0ce692c9d57064f
nao <8beesoftware@gmail.com>
2017-08-24 06:05:14 +0900

    stripe 支払い受取設定完了

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,15 +6,15 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.where(self_booking: nil)
   end
 
-  # def new
-  #   @listing = Listing.find(params[:listing_id])
-  #   @user = current_user
-  #
-  #   @start_date =  params[:reservation][:start_date]
-  #   @end_date =  params[:reservation][:end_date]
-  #   @price_pernight =  params[:reservation][:price_pernight]
-  #   @total_price =  params[:reservation][:total_price]
-  # end
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @user = current_user
+
+    @start_date =  params[:reservation][:start_date]
+    @end_date =  params[:reservation][:end_date]
+    @price_pernight =  params[:reservation][:price_pernight]
+    @total_price =  params[:reservation][:total_price]
+  end
 
   def reserved
     @listings = current_user.listings
@@ -49,7 +49,43 @@ class ReservationsController < ApplicationController
       redirect_to :back, notice: "更新しました。"
     else
 
-    @reservation = current_user.reservations.create(reservation_params)
+      #stripe
+      user = @listing.user
+
+      # Charge
+      amount = params[:reservation][:total_price]
+
+      #fee
+      fee = (amount.to_i * 0.1).to_i
+
+      # Calculate the fee amount that goes to the application.
+      # docs https://stripe.com/docs/connect/payments-fees
+      begin
+        charge_attrs = {
+            amount: amount,
+            currency: user.currency,
+            source: params[:token],
+            description: "Test Charge via Stripe Connect",
+            application_fee: fee
+        }
+
+        # Use the platform's access token, and specify the
+        # connected account's user id as the destination so that
+        # the charge is transferred to their account.
+        charge_attrs[:destination] = user.stripe_user_id
+
+        charge = Stripe::Charge.create( charge_attrs )
+
+        #have to edit view template to show html in flash
+        flash[:notice] = "Charged successfully!"
+
+      rescue Stripe::CardError => e
+        error = e.json_body[:error][:message]
+        flash[:error] = "Charge failed! #{error}"
+      end
+
+
+      @reservation = current_user.reservations.create(reservation_params)
       redirect_to @reservation.listing, notice: "予約が完了しました。"
     end
   end
